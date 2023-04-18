56f2ca8e7e7fbc1d455449afa1e1d12742fa8303
Daniel Carbajal <edudani81@gmail.com>
2023-04-17 23:31:51 -0600

    Files created to learn about react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
 import './index.css'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
@@ -13,12 +15,13 @@ import './index.css'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { CallbackHook } from './06-memos/CallbackHook'
 // import { Padre } from './07-tarea-memo/Padre'
-import { TodoApp } from './08-useReducer/TodoApp'
-
-// import './08-useReducer/intro-reducer'
+// import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TodoApp />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <MainApp />
+    </React.StrictMode>,
+  </BrowserRouter>
 )
