de96b521dfd6b6da7f3bde4efc38a282da7fe95f
Daniel Carbajal <edudani81@gmail.com>
2023-04-18 11:04:04 -0600

    Added context hooks in the app

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AboutPage, HomePage, LoginPage, NavBar } from "./";
+import { UserProvider } from "./context/UserProvider";
 
 export const MainApp = () => {
     return (
-        <>
+        <UserProvider>
             {/* <h1>MainApp</h1> */}
             <NavBar />
             <hr />
@@ -15,6 +16,6 @@ export const MainApp = () => {
 
                 <Route path="/*" element={<Navigate to="/about" />} />
             </Routes>
-        </>
+        </UserProvider>
     )
 }
