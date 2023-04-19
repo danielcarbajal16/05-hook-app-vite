0d262496b9032f8f14f973e604705a635b4c1472
Daniel Carbajal <edudani81@gmail.com>
2023-04-19 13:54:15 -0600

    TodoApp tests added

---
+++ tests/08-useReducer/TodoApp.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { TodoApp } from "../../src/08-useReducer";
+import { useTodos } from "../../src/hooks";
+
+jest.mock('../../src/hooks/useTodos');
+
+describe('Pruebas em el componente TodoApp', () => {
+    useTodos.mockReturnValue({
+        todos: [
+            { id: 1, description: 'Todo #1', done: false },
+            { id: 2, description: 'Todo #2', done: true }
+        ], 
+        todosCount: 2, 
+        pendingTodosCount: 1, 
+        onHandleNewToDo: jest.fn(), 
+        handleDeleteToDo: jest.fn(), 
+        handleToggleTodo: jest.fn()
+    });
+
+    test('Debe mostrar el componente correctamente', () => {
+        render( <TodoApp /> );
+
+        expect( screen.getByText('Todo #1') ).toBeTruthy();
+        expect( screen.getByText('Todo #2') ).toBeTruthy();
+        expect( screen.getByRole('textbox') ).toBeTruthy();
+    });
+})
